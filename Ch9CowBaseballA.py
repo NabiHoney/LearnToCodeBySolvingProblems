59b8160c22af4ef92e772fab20893d989598dfeb
Jena <116511683+NabiHoney@users.noreply.github.com>
2022-12-11 02:45:12 -0600

    Create Ch9CowBaseballA.py

---
+++ Ch9CowBaseballA.py
@@ -0,0 +1,18 @@
+input_file = open('baseball.in', 'r')
+output_file = open('baseball.out', 'w')
+
+n = int(input_file.readline())
+
+positions = []
+
+for i in range(n):
+    positions.append(int(input_file.readline()))
+
+total = 0
+
+for position1 in positions:
+    for position2 in positions:
+        first_two_diff = position2 - position1
+        if first_two_diff > 0:
+            low = position2 + first_two_diff
+            high = position2 + first_two_diff * 2
